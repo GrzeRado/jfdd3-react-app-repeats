cd6bad134b399caf04cf8108dd53413a0e804131
Grzegorz <rc36@wp.pl>
2018-01-29 12:12:51 +0100

    composeEnhancers add to store in constatnts

--- src/store.js
+++ src/store.js
@@ -8,5 +8,8 @@ const reducer = combineReducers({
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const store = createStore(
-    reducer
+    reducer,
+    composeEnhancers(
+        applyMiddleware(thunk)
+    )
 )
