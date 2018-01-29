945be9f14a5ef84995c323a4be4d3db7a9857528
Grzegorz <rc36@wp.pl>
2018-01-29 15:31:55 +0100

    fileter current user

--- src/App.js
+++ src/App.js
@@ -9,21 +9,22 @@ import {connect} from 'react-redux'
 
 const App = (props) => (
     <Router>
-    <div>
-        <Route path="/" exact render={() => (
-        <UsersList usersData={props.usersData}/>
-            )} />
-        /*<Route path="/user-details/:email" component={UserDetails}/>*/
-        <Route path="/user-details/:email" render={() => (
-            <UserDetails userData={props.userData}/>
-    </div>
-    </Router>
-)
+        <div>
+            <Route path="/" exact render={() => (
+                <UsersList usersData={props.usersData}/>
+            )}/>
+            /*<Route path="/user-details/:email" component={UserDetails}/>*/
+            <Route path="/user-details/:email" render={() => (
+                <UserDetails userData={props.userData}/>
+            )}
+        </div>
+        </Router>
+        )
 
-const mapStateToProps = state => ({
-    usersData: state.users.usersData
-})
+        const mapStateToProps = state => ({
+        usersData: state.users.usersData
+    })
 
-export default connect(
-    mapStateToProps
-)(App)
+        export default connect(
+        mapStateToProps
+        )(App)
