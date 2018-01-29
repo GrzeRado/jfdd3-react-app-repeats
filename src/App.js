70f388d6476131d9b83e89507e04a64c55ab65c4
Grzegorz <rc36@wp.pl>
2018-01-29 14:53:57 +0100

    Add browser router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import React from 'react'
+import {BrowserRouter as Router, Route} from 'react-router-dom'
+
 import UsersList from './components/UsersList'
+
 
 import {connect} from 'react-redux'
 
